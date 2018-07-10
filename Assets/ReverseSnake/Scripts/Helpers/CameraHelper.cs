5d22445b48c01bb43a2f7c14bfc1469493647eb4
nkitsan <nastik.kitsan@gmail.com>
2018-07-11 00:12:31 +0300

    Added: Fitting for all screensizes

---
+++ Assets/ReverseSnake/Scripts/Helpers/CameraHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHelper : MonoBehaviour
+{
+    void Awake ()
+    {
+        float aspectRatio = (float)Screen.currentResolution.height / (float)Screen.currentResolution.width;
+        if (aspectRatio <= 1.5f)
+        {
+            Camera.main.orthographicSize = 21.0f;
+        }
+        else if (aspectRatio <= 1.8f)
+        {
+            Camera.main.orthographicSize = 24.8f;
+        }
+        else if (aspectRatio <= 2.0f)
+        {
+            Camera.main.orthographicSize = 27.5f;
+        }
+        else
+        {
+            Camera.main.orthographicSize = 29.0f;
+        }
+    }
+}
