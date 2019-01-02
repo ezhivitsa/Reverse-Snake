eff83455e53000614c84f65450e06d5a51e3c7d8
Evgeny Zhivitsa <evgeny.zhivitsa@gmail.com>
2019-01-02 22:34:04 +0300

    [+] Add save settings

--- Assets/ReverseSnake/Scripts/Components/Settings/SettingsComponent.cs
+++ Assets/ReverseSnake/Scripts/Components/Settings/SettingsComponent.cs
@@ -1,15 +1,37 @@
 ﻿using Assets.ReverseSnake.Scripts.IO;
+using Assets.ReverseSnake.Scripts.Models;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SettingsComponent : MonoBehaviour
 {
-    public string mainMenu;
+    public string MainMenuScene;
+
+    private Toggle _musicToggle;
+    private Toggle _soundsToggle;
+
+    private SettingsModel _settings;
 
     void OnEnable()
     {
+        var toggleElements = GetComponentsInChildren<Toggle>();
+        _musicToggle = toggleElements[0];
+        _soundsToggle = toggleElements[1];
+
+        _musicToggle.onValueChanged.AddListener(MusicToggleValueChanged);
+        _soundsToggle.onValueChanged.AddListener(SoundsToggleValueChanged);
+
         SaveSettings.OnLoaded += OnSettingsLoaded;
         SaveSettings.Load();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            MainMenu();
+        }
     }
 
     void OnDestroy()
@@ -19,11 +41,46 @@ public class SettingsComponent : MonoBehaviour
 
     public void MainMenu()
     {
-        SceneManager.LoadScene(mainMenu);
+        SceneManager.LoadScene(MainMenuScene);
     }
 
     private void OnSettingsLoaded()
     {
-        var settings = SaveSettings.Settings;
+        _settings = SaveSettings.Settings;
+
+        _musicToggle.isOn = _settings.UseMusic;
+        _soundsToggle.isOn = _settings.UseSounds;
+    }
+
+    private void MusicToggleValueChanged(bool isOn)
+    {
+        if (_settings == null)
+        {
+            return;
+        }
+
+        var settings = new SettingsModel
+        {
+            UseMusic = isOn,
+            UseSounds = _settings.UseSounds,
+        };
+        _settings = settings;
+        SaveSettings.Save(settings);
+    }
+
+    private void SoundsToggleValueChanged(bool isOn)
+    {
+        if (_settings == null)
+        {
+            return;
+        }
+
+        var settings = new SettingsModel
+        {
+            UseMusic = _settings.UseMusic,
+            UseSounds = isOn,
+        };
+        _settings = settings;
+        SaveSettings.Save(settings);
     }
 }
